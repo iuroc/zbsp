57f402ea81e0320bec8bd312c950d33f997d424a
oyps <ponconsoft@gmail.com>
2022-10-16 03:15:21 +0800

    初始化

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 配置文件
+ */
+
+$config = [
+    'title' => '主播视频', // 网站标题
+    'description' => '集影视、小说、图集为一体的在线资源共享平台', // 网站描述
+    'logo_url' => 'img/logo-blue.png', // 网站logo URL
+    'mysql' => [
+        'host' => 'localhost', // 数据库地址
+        'user' => 'root', // 数据库用户名
+        'pass' => '', // 数据库密码
+        'db' => '', // 数据库名称
+    ],
+    'table' => [
+        'key' => 'website_key', // 文件表名
+    ],
+];
