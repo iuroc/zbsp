b5dd92b7429580cd488c5e9603ab21cb9632c19a
oyps <ponconsoft@gmail.com>
2022-10-20 22:42:13 +0800

    优化域名更新程序

--- api/get_domain.php
+++ api/get_domain.php
@@ -7,10 +7,6 @@
 function get_domain()
 {
     stream_context_set_default([
-        // 'http' => [
-        //     'proxy' => 'tcp://127.0.0.1:7890',
-        //     'request_fulluri' => True
-        // ],
         'ssl' => [
             'verify_peer' => false,
             'verify_peer_name' => false,
@@ -29,16 +25,25 @@ function get_domain()
     $update_at = $domain_info['update_at'];
     // 判断是否过期 1天更新一次
     if (time() - $update_at > 24 * 60 * 60) {
-        $headers = get_headers($domain_info['domain']);
+        // $headers = get_headers($domain_info['domain']);
+        $headers = get_headers('https://web.dxj.mobi/');
         $domain_info['update_at'] = time();
+        $flag = false;
         foreach ($headers as $item) {
             preg_match('/location.*?:.*?(http.*)/i', $item, $matches);
             if (isset($matches[1])) {
                 $domain_info['domain'] = $matches[1];
+                $flag = true;
                 break;
             }
+        }
+        if (!$flag) {
+            return 'error';
         }
         file_put_contents('domain_info.json', json_encode($domain_info));
     }
     return $domain_info['domain'];
+}
+if (isset($_GET['run']) && $_GET['run'] == 'true') {
+    echo get_domain();
 }
